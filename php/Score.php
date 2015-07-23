64643d8607b0c34f582e2d3fa0958fcb4c5895e7
Luis Rovirosa <luisrovirosa@gmail.com>
2015-07-23 18:05:34 +0200

    TennisGame1 Score class created

---
+++ php/Score.php
@@ -0,0 +1,77 @@
+<?php
+
+class Score
+{
+    /** @var Player */
+    private $player1;
+
+    /** @var Player */
+    private $player2;
+
+    /**
+     * Score constructor.
+     * @param $player1
+     * @param $player2
+     */
+    public function __construct($player1, $player2)
+    {
+        $this->player1 = $player1;
+        $this->player2 = $player2;
+    }
+
+    public function toString()
+    {
+        $score = "";
+        if ($this->player1->scores() == $this->player2->scores()) {
+            switch ($this->player1->scores()) {
+                case 0:
+                    $score = "Love-All";
+                    break;
+                case 1:
+                    $score = "Fifteen-All";
+                    break;
+                case 2:
+                    $score = "Thirty-All";
+                    break;
+                default:
+                    $score = "Deuce";
+                    break;
+            }
+        } elseif ($this->player1->scores() >= 4 || $this->player2->scores() >= 4) {
+            $minusResult = $this->player1->scores() - $this->player2->scores();
+            if ($minusResult == 1) {
+                $score = "Advantage player1";
+            } elseif ($minusResult == -1) {
+                $score = "Advantage player2";
+            } elseif ($minusResult >= 2) {
+                $score = "Win for player1";
+            } else {
+                $score = "Win for player2";
+            }
+        } else {
+            for ($i = 1; $i < 3; $i++) {
+                if ($i == 1) {
+                    $tempScore = $this->player1->scores();
+                } else {
+                    $score .= "-";
+                    $tempScore = $this->player2->scores();
+                }
+                switch ($tempScore) {
+                    case 0:
+                        $score .= "Love";
+                        break;
+                    case 1:
+                        $score .= "Fifteen";
+                        break;
+                    case 2:
+                        $score .= "Thirty";
+                        break;
+                    case 3:
+                        $score .= "Forty";
+                        break;
+                }
+            }
+        }
+        return $score;
+    }
+}
--- php/TennisGame1.php
+++ php/TennisGame1.php
@@ -6,11 +6,14 @@ class TennisGame1 implements TennisGame
     private $player1;
     /** @var  Player */
     private $player2;
+    /** @var Score */
+    private $score;
 
     public function __construct($player1Name, $player2Name)
     {
         $this->player1 = new Player($player1Name);
         $this->player2 = new Player($player2Name);
+        $this->score = new Score($this->player1, $this->player2);
     }
 
     public function wonPoint($playerName)
@@ -25,58 +28,7 @@ class TennisGame1 implements TennisGame
 
     public function getScore()
     {
-        $score = "";
-        if ($this->player1->scores() == $this->player2->scores()) {
-            switch ($this->player1->scores()) {
-                case 0:
-                    $score = "Love-All";
-                    break;
-                case 1:
-                    $score = "Fifteen-All";
-                    break;
-                case 2:
-                    $score = "Thirty-All";
-                    break;
-                default:
-                    $score = "Deuce";
-                    break;
-            }
-        } elseif ($this->player1->scores() >= 4 || $this->player2->scores() >= 4) {
-            $minusResult = $this->player1->scores() - $this->player2->scores();
-            if ($minusResult == 1) {
-                $score = "Advantage player1";
-            } elseif ($minusResult == -1) {
-                $score = "Advantage player2";
-            } elseif ($minusResult >= 2) {
-                $score = "Win for player1";
-            } else {
-                $score = "Win for player2";
-            }
-        } else {
-            for ($i = 1; $i < 3; $i++) {
-                if ($i == 1) {
-                    $tempScore = $this->player1->scores();
-                } else {
-                    $score .= "-";
-                    $tempScore = $this->player2->scores();
-                }
-                switch ($tempScore) {
-                    case 0:
-                        $score .= "Love";
-                        break;
-                    case 1:
-                        $score .= "Fifteen";
-                        break;
-                    case 2:
-                        $score .= "Thirty";
-                        break;
-                    case 3:
-                        $score .= "Forty";
-                        break;
-                }
-            }
-        }
-        return $score;
+        return $this->score->toString();
     }
 }
 
